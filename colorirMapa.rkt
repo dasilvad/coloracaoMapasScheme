e4ef764b02e860680117af8298c8ae6c97e43b6e
daniel <silva10.daniel@gmail.com>
2015-11-18 00:27:35 -0200

    lista de estados

---
+++ colorirMapa.rkt
@@ -0,0 +1,31 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname colorirMapa) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
+{"BA", "SE", "AL", "PE", "PI", "MA", "TO", "GO", "MG", "ES"},
+{"MG", "GO", "MS", "SP", "RJ", "ES", "BA"},
+{"PA", "AP", "RR", "AM", "MT", "TO", "MA"},
+{"MT", "PA","AM", "RO", "MS", "GO", "TO"},
+{"PE", "PB", "CE", "PI", "BA", "AL"},
+{"GO", "BA", "TO", "MT", "MS", "MG"},
+{"TO", "MA", "PA", "MT", "GO", "BA"},
+{"MS", "PR", "SP", "GO", "MG", "MT"},
+{"AM", "RR", "AC", "MT", "RO", "PA"},
+{"MA", "PA", "TO", "BA", "PI"},
+{"PI", "MA", "BA", "PE", "CE"},
+{"CE", "PI", "PE", "PB", "RN"},
+{"SP", "PR", "MS", "RJ", "MG"},
+{"PR", "SC", "SP", "MS"},
+{"RJ", "SP", "MG", "ES"},
+{"ES", "MG", "RJ", "BA"},
+{"RO", "AM", "AC", "MT"},
+{"PB", "RN", "CE", "PE"},
+{"AL", "PE", "BA", "SE"},
+{"SC", "RS", "PR"},
+{"AC", "RO", "AM"},
+{"RR", "PA", "AM"},
+{"RN", "CE", "PB"},
+{"SE", "AL", "BA"} 
+{"RS", "SC"},
+{"AP", "PA"}
+ 
+
